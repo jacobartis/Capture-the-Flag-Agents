38803a47e342b15873dab839792ca1e98b492753
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-30 18:18:17 +0100

    Added method to generate a grid of spaces.

--- environment.py
+++ environment.py
@@ -1,5 +1,6 @@
 import mesa
 from agent import Agent
+from space import Space
 from time import sleep
 
 def main():
@@ -7,6 +8,13 @@ def main():
     while True:
         environment.step()
         sleep(1)
+
+def generate_grid(x,y):
+    spaces = {}
+    for i in range(x):
+        for j in range(y):
+            spaces[(i,j)] = Space()
+    return spaces
 
 class Enviornment(mesa.Model):
 
@@ -17,13 +25,8 @@ class Enviornment(mesa.Model):
         
         self.schedule.add(Agent(1, self, (0,0)))
 
-        self.grid = {
-            (0,0):0,(1,0):0,(2,0):0,(3,0):0,(4,0):0,
-            (0,1):0,(1,1):0,(2,1):0,(3,1):0,(4,1):0,
-            (0,2):0,(1,2):0,(2,2):0,(3,2):0,(4,2):0,
-            (0,3):0,(1,3):0,(2,3):0,(3,3):0,(4,3):0,
-            (0,4):0,(1,4):0,(2,4):0,(3,4):0,(4,4):0,
-        }
+        self.grid = generate_grid(5,5)
+        print(self.grid)
         # 0 = Off
         # 1 = On
 
