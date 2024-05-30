f5a234c0aac66df450f590078877f06e1eb6f4e0
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-30 12:50:16 +0100

    Added the Mesa library to framework the agents and environment.

--- agent.py
+++ agent.py
@@ -1,7 +1,16 @@
+import mesa
 
-class Agent():
+class Agent(mesa.Agent):
 
-    coordinates = (0,0)
+    def __init__(self, unique_id, model, start_pos=(0,0)):
+        super().__init__(unique_id, model)
+        self.coordinates = start_pos
 
-    def action():
-        return
+    def step(self):
+        print(f"{str(self.unique_id)} stepped, pos:{str(self.coordinates)}")
+        self.move()
+
+    def move(self):
+        grid = self.model.grid
+        assert (self.coordinates[0]+1,self.coordinates[1]) in grid, "Trying to move to an invalid space"
+        self.coordinates = (self.coordinates[0]+1,self.coordinates[1])
--- environment.py
+++ environment.py
@@ -1,36 +1,37 @@
+import mesa
 from agent import Agent
 from time import sleep
 
 def main():
-    environment = Enviornment([Agent])
+    environment = Enviornment()
     while True:
-        environment.cycle()
+        environment.step()
         sleep(1)
 
-class Enviornment():
-    grid = {
-        (0,0):0,(1,0):0,(2,0):0,(3,0):0,(4,0):0,
-        (0,1):0,(1,1):0,(2,1):0,(3,1):0,(4,1):0,
-        (0,2):0,(1,2):0,(2,2):0,(3,2):0,(4,2):0,
-        (0,3):0,(1,3):0,(2,3):0,(3,3):0,(4,3):0,
-        (0,4):0,(1,4):0,(2,4):0,(3,4):0,(4,4):0,
-    }
-    # 0 = Off
-    # 1 = On
+class Enviornment(mesa.Model):
 
-    agents: list[Agent] = []
+    def __init__(self):
+        super().__init__()
 
-    def __init__(self,agents) -> None:
-        self.agents = agents
+        self.schedule = mesa.time.RandomActivation(self)
+        
+        self.schedule.add(Agent(1, self, (1,1)))
 
-    def move_agent(self, agent, new_coordinates):
-        assert agent.coordinates in self.grid and new_coordinates in self.grid
-        agent.coordinates = new_coordinates
-        print(agent.coordinates)
+        self.grid = {
+            (0,0):0,(1,0):0,(2,0):0,(3,0):0,(4,0):0,
+            (0,1):0,(1,1):0,(2,1):0,(3,1):0,(4,1):0,
+            (0,2):0,(1,2):0,(2,2):0,(3,2):0,(4,2):0,
+            (0,3):0,(1,3):0,(2,3):0,(3,3):0,(4,3):0,
+            (0,4):0,(1,4):0,(2,4):0,(3,4):0,(4,4):0,
+        }
+        # 0 = Off
+        # 1 = On
 
-    def cycle(self):
-        for agent in self.agents:
-            self.move_agent(agent=agent,new_coordinates=(agent.coordinates[0]+1,agent.coordinates[1]))
+    def change_cell(self,cell):
+        self.grid[cell] = (self.grid[cell]+1)%1
+
+    def step(self):
+        self.schedule.step()
 
 if __name__ == "__main__":
     main()
