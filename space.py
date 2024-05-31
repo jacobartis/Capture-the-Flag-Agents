27277ab0bb9bb139f71311ab48165a0de938e2dc
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-31 20:11:34 +0100

    Added now tracks current object and agent.

--- space.py
+++ space.py
@@ -1,15 +1,26 @@
 from agent import Agent
 from coordinates import Coordinates
+from ctf_object import CTFObject
 
 class Space():
 
     def __init__(self, coordinates:Coordinates):
         self.agent = None
+        self.object = None
         self.coordinates = coordinates
-
-    def add_agent(self,agent):
-        self.agent = agent
     
+    def set_object(self,obj:CTFObject):
+        self.object = obj
+    
+    def get_object(self):
+        return self.object
+
+    def set_agent(self,agent:Agent):
+        self.agent = agent
+        agent.space = self
+    
+    def remove_agent(self):
+        self.agent = None
+
     def get_agent(self):
         return self.agent
-    
